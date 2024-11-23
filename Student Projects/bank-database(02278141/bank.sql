422816bffc9e7350281658d438c3a8d5f8409e5e
Alizada20 <aliaalizada2022@gmail.com>
2024-11-23 11:57:52 +0430

    Add files via upload
    this database contains all information about bank

---
+++ Student Projects/bank-database(02278141/bank.sql
@@ -0,0 +1,167 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bank
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `account`
+--
+
+DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `account` (
+  `Acc_No` int NOT NULL,
+  `Creation_Date` date DEFAULT NULL,
+  `Acc_Type` varchar(20) NOT NULL,
+  `Balance` float DEFAULT NULL,
+  `D_ID` int DEFAULT NULL,
+  `B_ID` int NOT NULL,
+  PRIMARY KEY (`Acc_No`),
+  KEY `D_ID` (`D_ID`),
+  KEY `B_ID` (`B_ID`),
+  CONSTRAINT `account_ibfk_1` FOREIGN KEY (`D_ID`) REFERENCES `depositor` (`D_ID`),
+  CONSTRAINT `account_ibfk_2` FOREIGN KEY (`D_ID`) REFERENCES `depositor` (`D_ID`),
+  CONSTRAINT `account_ibfk_3` FOREIGN KEY (`B_ID`) REFERENCES `branch` (`B_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account`
+--
+
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+INSERT INTO `account` VALUES (1,'2024-02-13','business',23000,1,1);
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `branch`
+--
+
+DROP TABLE IF EXISTS `branch`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `branch` (
+  `B_ID` int NOT NULL,
+  `B_name` varchar(20) NOT NULL,
+  `country` varchar(20) DEFAULT NULL,
+  `province` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`B_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `branch`
+--
+
+LOCK TABLES `branch` WRITE;
+/*!40000 ALTER TABLE `branch` DISABLE KEYS */;
+INSERT INTO `branch` VALUES (1,'Shahr-e-Naw','Afghanistan','kabul');
+/*!40000 ALTER TABLE `branch` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contact` (
+  `D_PhoneNO` int NOT NULL,
+  `D_ID` int NOT NULL,
+  UNIQUE KEY `D_PhoneNO` (`D_PhoneNO`),
+  KEY `D_ID` (`D_ID`),
+  CONSTRAINT `contact_ibfk_1` FOREIGN KEY (`D_ID`) REFERENCES `depositor` (`D_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contact`
+--
+
+LOCK TABLES `contact` WRITE;
+/*!40000 ALTER TABLE `contact` DISABLE KEYS */;
+INSERT INTO `contact` VALUES (78885455,1);
+/*!40000 ALTER TABLE `contact` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `depositor`
+--
+
+DROP TABLE IF EXISTS `depositor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `depositor` (
+  `D_ID` int NOT NULL AUTO_INCREMENT,
+  `D_Name` varchar(20) NOT NULL,
+  `Email` varchar(20) NOT NULL,
+  PRIMARY KEY (`D_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `depositor`
+--
+
+LOCK TABLES `depositor` WRITE;
+/*!40000 ALTER TABLE `depositor` DISABLE KEYS */;
+INSERT INTO `depositor` VALUES (1,'Pasoon','pasoonbarakzai2024');
+/*!40000 ALTER TABLE `depositor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `loan`
+--
+
+DROP TABLE IF EXISTS `loan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `loan` (
+  `loan_No` int NOT NULL,
+  `amount` int DEFAULT NULL,
+  `B_ID` int NOT NULL,
+  `D_ID` int NOT NULL,
+  UNIQUE KEY `loan_No` (`loan_No`),
+  KEY `B_ID` (`B_ID`),
+  KEY `D_ID` (`D_ID`),
+  CONSTRAINT `loan_ibfk_1` FOREIGN KEY (`B_ID`) REFERENCES `branch` (`B_ID`),
+  CONSTRAINT `loan_ibfk_2` FOREIGN KEY (`D_ID`) REFERENCES `depositor` (`D_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `loan`
+--
+
+LOCK TABLES `loan` WRITE;
+/*!40000 ALTER TABLE `loan` DISABLE KEYS */;
+INSERT INTO `loan` VALUES (1,2400,1,1);
+/*!40000 ALTER TABLE `loan` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-11-23 11:12:57
